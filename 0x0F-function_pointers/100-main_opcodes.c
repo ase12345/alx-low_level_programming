bff57387029e45fa54a594ba5b9fac2e71d7ed5b
ase12345 <asefa.mulu54@gmail.com>
2022-10-12 13:40:30 +0300

    a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,40 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
 
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
-int main(int argc, char *argv[])
+ * main - prints the opcodes of its own main function
+ *
+ * @argv: the argument that enters to the program
+ * @argc: counts the number of arguments
+ *
+ * Return: the opcode hex number
+ */
+int main(int argc, char **argv)
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+int runner = 0, bytes;
+/* the tasks tell us each opcode is two char long */
+/* if the argv is less or more then Error */
 
-	if (argc == 2)
-	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
-	}
-
-	return (0);
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
 }
-
+/* the argument in position one (the number the program receives) */
+/* will be equal to bytes */
+bytes = atoi(argv[1]);
+/* condition if the number of bytes y negative (less than cero) */
+/* exit status 2 and print Error */
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+/* we use runner to go through the array we receive in the argv */
+while (runner < bytes)
+{
+/* is pre define how to print an opcodes */
+printf("%02hhx", *((char *)(main + runner)));
+if (bytes > runner + 1)
+printf(" ");
+runner++;
+}
+printf("\n");
+return (0);
+}
